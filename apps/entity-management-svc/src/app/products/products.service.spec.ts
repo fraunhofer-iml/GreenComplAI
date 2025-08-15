b15bad39323e1416f315b98811dde46cca8918fc
Matthias Schönborn <matthias.schoenborn@iml.fraunhofer.de>
2025-08-15 15:05:02 +0200

    refactor: enhance styling of uploaded files header for consistency

--- apps/entity-management-svc/src/app/products/products.service.spec.ts
+++ apps/entity-management-svc/src/app/products/products.service.spec.ts
@@ -8,11 +8,16 @@
 
 import { ConfigurationModule } from '@ap2/configuration';
 import { DatabaseModule, PrismaService } from '@ap2/database';
+import {
+  STORAGE_SERVICE_TOKEN,
+  StorageMinioService,
+} from '@greencomplai/storage';
 import { Test, TestingModule } from '@nestjs/testing';
 import { FlagsModule } from '../flags/flags.module';
 import { WasteModule } from '../waste/waste.module';
 import { ProductAnalysisService } from './analysis.service';
 import { ProductCrudService } from './product-crud.service';
+import { ProductFileService } from './product-file.service';
 import { ProductOutlierService } from './product-outlier.service';
 import { ProductRelationsService } from './product-relations.service';
 import { ProductSupplierService } from './product-supplier.service';
@@ -31,7 +36,16 @@ describe('ProductService', () => {
         ProductOutlierService,
         ProductSupplierService,
         ProductService,
+        ProductFileService,
         { provide: PrismaService, useValue: {} },
+        {
+          provide: STORAGE_SERVICE_TOKEN,
+          useValue: {
+            uploadFile: jest.fn(),
+            deleteFile: jest.fn(),
+            getFileUrl: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
