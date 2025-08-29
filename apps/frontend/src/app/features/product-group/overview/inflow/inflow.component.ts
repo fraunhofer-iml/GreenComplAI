6e919a55bbdea5fbcf292f247250a443e9f7d6f5
Eva-Maria Stetter <eva-maria.stetter@iml.fraunhofer.de>
2025-08-29 11:20:39 +0200

    remove pagination

--- apps/frontend/src/app/features/product-group/overview/inflow/inflow.component.ts
+++ apps/frontend/src/app/features/product-group/overview/inflow/inflow.component.ts
@@ -29,6 +29,7 @@ import {
   MatDatepicker,
   MatDatepickerModule,
 } from '@angular/material/datepicker';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -60,6 +61,7 @@ import { ONLY_YEAR_FORMAT } from '../../../../shared/constants/date-formats';
     MatDatepickerModule,
     MatNativeDateModule,
     MatPaginatorModule,
+    MatExpansionModule,
   ],
   providers: [
     {
