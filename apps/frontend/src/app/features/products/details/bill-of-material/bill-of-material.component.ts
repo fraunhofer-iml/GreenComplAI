99c9fd18f4de08599c63d49349cd85226366a2b4
Eva-Maria Stetter <eva-maria.stetter@iml.fraunhofer.de>
2025-09-09 16:37:32 +0200

    add more pipes

--- apps/frontend/src/app/features/products/details/bill-of-material/bill-of-material.component.ts
+++ apps/frontend/src/app/features/products/details/bill-of-material/bill-of-material.component.ts
@@ -9,6 +9,7 @@
 import { ProductDto, ProductUpdateMapDto } from '@ap2/api-interfaces';
 import { toast } from 'ngx-sonner';
 import { elementAt } from 'rxjs';
+import { DecimalPipe } from '@angular/common';
 import { Component, inject, input, OnChanges } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -35,6 +36,7 @@ import { BillOfMaterialFormGroup } from '../../create/model/product-form.model';
     MatIconModule,
     MatButtonModule,
     FlagableComponent,
+    DecimalPipe,
   ],
   templateUrl: './bill-of-material.component.html',
 })
