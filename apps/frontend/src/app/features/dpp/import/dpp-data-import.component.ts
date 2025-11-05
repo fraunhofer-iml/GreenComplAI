45326ea65e0694f15ae269db1ba404f744b698e3
Eva-Maria Stetter <eva-maria.stetter@iml.fraunhofer.de>
2025-11-05 11:37:42 +0100

    add update endpoint connection

--- apps/frontend/src/app/features/dpp/import/dpp-data-import.component.ts
+++ apps/frontend/src/app/features/dpp/import/dpp-data-import.component.ts
@@ -6,7 +6,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { ProductDto } from '@ap2/api-interfaces';
+import {
+  CriticalRawMaterials,
+  ProductDto,
+  ProductMasterDataDto,
+  ProductUpdateDto,
+} from '@ap2/api-interfaces';
+import { toast } from 'ngx-sonner';
 import { CommonModule } from '@angular/common';
 import { Component, computed, inject, input } from '@angular/core';
 import {
@@ -15,8 +21,13 @@ import {
   FormsModule,
   ReactiveFormsModule,
 } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { injectQuery } from '@tanstack/angular-query-experimental';
+import { Router } from '@angular/router';
+import {
+  injectMutation,
+  injectQuery,
+} from '@tanstack/angular-query-experimental';
 import { DppService } from '../../../core/services/dpp/dpp.service';
 import { ProductsService } from '../../../core/services/products/products.service';
 import { ProductDppCompareComponent } from './comparison/dpp-card.component';
@@ -31,6 +42,7 @@ import { ProductCardComponent } from './comparison/product-card.component';
     ReactiveFormsModule,
     MatCheckboxModule,
     ProductCardComponent,
+    MatButtonModule,
   ],
   providers: [DppService],
   templateUrl: './dpp-data-import.component.html',
@@ -38,6 +50,8 @@ import { ProductCardComponent } from './comparison/product-card.component';
 export class DppDataImportComponent {
   private readonly dppService = inject(DppService);
   private readonly productService = inject(ProductsService);
+  private readonly router = inject(Router);
+
   id = input<string>('id');
 
   dppQuery = injectQuery(() => ({
@@ -98,7 +112,72 @@ export class DppDataImportComponent {
     productCarbonFootprint: new FormControl<boolean>(false),
   });
 
+  updateMutation = injectMutation(() => ({
+    mutationFn: (dto: ProductUpdateDto) => {
+      return this.productService.update(dto, this.id() ?? '');
+    },
+    onSuccess: () => this.router.navigate(['/products', this.id()]),
+    onError: () => toast.error('Speichern fehlgeschlagen'),
+  }));
+
   constructor() {
     this.formGroup.valueChanges.subscribe((val) => console.log(val));
+  }
+
+  submit() {
+    const data = this.dppQuery.data();
+    if (!data) return;
+    console.log(this.formGroup.value);
+    const formValue = this.formGroup.value;
+
+    const dto = {} as ProductUpdateDto;
+    const masterData = {} as ProductMasterDataDto;
+
+    if (formValue.name) masterData.name = data.name;
+
+    if (formValue.taricCode) masterData.taricCode = data.taricCode;
+
+    if (formValue.gtin) masterData.gtin = data.gtin;
+
+    if (formValue.waterUsed) masterData.waterUsed = data.waterUsed;
+
+    if (formValue.productId) masterData.productId = data.productId;
+    if (formValue.productCarbonFootprint)
+      masterData.productCarbonFootprint = data.productCarbonFootprint;
+
+    if (formValue.supplier) {
+      // TODO:  create company
+    }
+
+    if (formValue.importer) {
+      masterData.importerName = data.importerName;
+      masterData.importerPhone = data.importerPhone;
+      masterData.importerEmail = data.importerEmail;
+      masterData.importerAddress = data.importerAddress;
+    }
+
+    dto.masterData = masterData;
+
+    if (formValue.materials)
+      dto.materials = (data.materials ?? []).map((m) => ({
+        material: m[0].name,
+        percentage: m[1],
+        primary: m[2],
+        renewable: m[3],
+      }));
+
+    if (formValue.criticalRawMaterials) {
+      dto.criticalRawMaterials = [];
+      (data.criticalRawMaterials ?? []).forEach((m) => {
+        dto.criticalRawMaterials.push({
+          material: m[0].name as CriticalRawMaterials,
+          percentage: m[1],
+        });
+      });
+    }
+
+    console.log(dto);
+
+    this.updateMutation.mutate(dto);
   }
 }
